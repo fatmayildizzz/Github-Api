2ad9a9794da9c65b3dfbf16e84834dd64199267c
Fatma <110012833+fatmayildizzz@users.noreply.github.com>
2022-08-19 15:13:23 +0300

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,32 @@
+class Storage {
+
+    static getSearchedUsersFromStorage(){
+        //tum kullanicilari al
+
+        let users;
+        if(localStorage.getItem("searched")=== null){
+            users= [];
+        }
+        else{
+            users=JSON.parse(localStorage.getItem("searched"));
+        }
+        return users;
+    }
+    static addSearchedUserToStorage(username){
+
+        //Kullanici ekle
+        let users= this.getSearchedUsersFromStorage();
+
+        if(users.indexOf(username)=== -1){
+            users.push(username);
+        }
+        localStorage.setItem("searched",JSON.stringify(users));
+    }
+    static clearAllSearchedUsersFromStorage(){
+
+        
+        localStorage.removeItem("searched");
+
+
+    }
+}
